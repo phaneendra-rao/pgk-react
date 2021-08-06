c2069b510ca390c23f62106f362e11aeb829cb09
VenkatPrasanna-glitch <venkatprasanna619@gmail.com>
2021-08-06 13:58:20 +0530

    reverting the changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://api.c2hire.com/',
-      changeOrigin: true,
-      prependPath: false
-    })
-  );
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'http://api.c2hire.com/',
+//       changeOrigin: true,
+//       prependPath: false
+//     })
+//   );
 };
